3f086cb099a6169f7edefe3a10f9e7da718b361f
Manish Pandey <137053707+astromanreal@users.noreply.github.com>
2025-05-09 15:40:12 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+      <meta name="google-site-verification" content="mac7aLjz9hgBPOeatEJp8fZ6RL2GRi8PeWQfgcITzFU" />
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
